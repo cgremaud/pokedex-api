e933e2be775329f869638641f0dca72f35644c77
colyn <colyn.gremaud@gmail.com>
2022-11-24 08:36:43 -0600

    implemented func to fetch pokemon type info but it's not working.

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -11,8 +11,27 @@ const getPokemon = async function(name = "ditto") {
 const getPokemonAbilities = async function(name) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     const pokemon = await response.json();
-    const abilities = pokemon.abilities;
-    return abilities;   
+    const result = {};
+    result.abilities = pokemon.abilities;
+    result.name = name;
+    return result;   
+}
+
+const getPokemonTypeInfo = async function(name) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    const pokemon = await response.json();
+    const types = pokemon.types;
+    const result = {
+        name: name, 
+        types: []
+    };
+    let type;
+    for (let i = 0; i < types.length; i++) {
+      type = await fetch(types[i].type.url);  
+      console.log(type) 
+      result.types.push(type);
+    } 
+    return result;
 }
 
 router.get('/:name', async function(req, res, next) {
@@ -23,7 +42,11 @@ router.get('/:name', async function(req, res, next) {
 router.get('/abilities/:name', async function(req, res, next) {
     const abilities = await getPokemonAbilities(req.params.name);
     res.send(abilities);
+})
 
+router.get('/types/:name', async function(req, res, next) {
+    const types = await getPokemonTypeInfo(req.params.name);
+    res.send(types);
 })
 
 module.exports = router;
