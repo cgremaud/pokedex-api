d4985e266995ff08553251d10f35391eddaab5f4
colyn <colyn.gremaud@gmail.com>
2022-11-24 13:27:31 -0600

    find all in range func now works, priority order issue caused problem

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,14 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 const getPokemon = async function(name = "ditto") {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
     const pokemon = await response.json();
     const abilities = pokemon.abilities;
     return pokemon;    
 }
 
+const getAllPokemonInRange = async function(limit = 100, offset= 0) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=0`);
+    const pokemon = await response.json();
+    return pokemon;
+}
+
 const getPokemonAbilities = async function(name) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
     const pokemon = await response.json();
     const result = {};
     result.abilities = pokemon.abilities;
@@ -18,7 +24,7 @@ const getPokemonAbilities = async function(name) {
 }
 
 const getPokemonTypeInfo = async function(name) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
     const pokemon = await response.json();
     const types = pokemon.types;
     let result = {
@@ -31,10 +37,20 @@ const getPokemonTypeInfo = async function(name) {
       typeResult = await fetch(types[i].type.url); 
       type = await typeResult.json(); 
       console.log(type) 
-      result.types.push(type);
+      result.types.push({
+            name: types[i].type.name,
+            typeInfo: type
+        });
     } 
     return result;
 }
+
+router.get('/findall', async function(req, res, next) {
+    const limit = req.query.limit;
+    const offset = req.query.offset;
+    const range = await getAllPokemonInRange(limit, offset);
+    res.send(range);
+})
 
 router.get('/:name', async function(req, res, next) {
     const pokemon = await getPokemon(req.params.name);
@@ -50,5 +66,7 @@ router.get('/types/:name', async function(req, res, next) {
     const types = await getPokemonTypeInfo(req.params.name);
     res.send(types);
 })
+
+
 
 module.exports = router;
