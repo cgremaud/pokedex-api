32f3b51e19d62d5f7cc6b2c6450b0a3359409728
colyn <colyn.gremaud@gmail.com>
2022-11-29 22:44:04 -0600

    wrapped getters in try/catch. can't figure out how to import/export modules

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,50 +1,76 @@
+
 var express = require('express');
 var router = express.Router();
 
-//todo break these functions out into a separate module 
+// const getterFunctions = require("../api/pokemon")
+
+//this wasn't working   
+// const getPokemon = getterFunctions.getPokemon;
+// const getAllPokemonInRange = getterFunctions.getAllPokemonInRange;
+// const getPokemonAbilities = getterFunctions.getPokemonAbilities;
+// const getPokemonTypeInfo = getterFunctions.getPokemonTypeInfo;
+
+// todo break these functions out into a separate module 
 const getPokemon = async function(name = "ditto") {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    const pokemon = await response.json();
-    return pokemon;    
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const pokemon = await response.json();
+        return pokemon;
+    } catch(err) {
+        console.error(err);
+    }    
 }
 
 const getAllPokemonInRange = async function(limit = 100, offset= 0) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
-    const pokemon = await response.json();
-    return pokemon;
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+        const pokemon = await response.json();
+        return pokemon;
+    } catch(err) {
+        console.error(err);
+    }
 }
 
 const getPokemonAbilities = async function(name) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    const pokemon = await response.json();
-    const result = {};
-    result.abilities = pokemon.abilities;
-    result.name = name;
-    return result;   
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const pokemon = await response.json();
+        const result = {};
+        result.abilities = pokemon.abilities;
+        result.name = name;
+        return result;
+    } catch(err) {
+        console.error(err);
+    }
 }
 
 //TODO modify this to include past type data and maybe correlate to which type it had in which generation
 const getPokemonTypeInfo = async function(name) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    const pokemon = await response.json();
-    const types = pokemon.types;
-    let result = {
-        name: name, 
-        types: []
-    };
-    let type;
-    let typeResult;
-    for (let i = 0; i < types.length; i++) {
-      typeResult = await fetch(types[i].type.url); 
-      type = await typeResult.json(); 
-      console.log(type) 
-      result.types.push({
-            name: types[i].type.name,
-            typeInfo: type
-        });
-    } 
-    return result;
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const pokemon = await response.json();
+        const types = pokemon.types;
+        let result = {
+            name: name, 
+            types: []
+        };
+        let type;
+        let typeResult;
+        for (let i = 0; i < types.length; i++) {
+            typeResult = await fetch(types[i].type.url); 
+            type = await typeResult.json(); 
+            console.log(type) 
+            result.types.push({
+                    name: types[i].type.name,
+                    typeInfo: type
+                });
+        } 
+        return result;
+   } catch(err) {
+        console.error(err);
+   }
 }
+
 
 router.get('/findall', async function(req, res, next) {
     const limit = req.query.limit;
