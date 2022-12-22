ff9f675cf8dd93fd3bed125cc6f596fdb08ca64a
colyn <colyn.gremaud@gmail.com>
2022-12-21 20:01:22 -0600

    refactored error handling. 404s work now. yay.

--- api/pokemon.js
+++ api/pokemon.js
@@ -1,61 +1,45 @@
 const getPokemon = async function(name = "ditto") {
-    try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-        const pokemon = await response.json();
-        return pokemon;
-    } catch(err) {
-
-    }    
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const pokemon = await response.json();
+    return pokemon;
 }
 
 const getAllPokemonInRange = async function(limit = 100, offset= 0) {
-    try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
-        const pokemon = await response.json();
-        return pokemon;
-    } catch(err) {
-
-    }
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+    const pokemon = await response.json();
+    return pokemon;
 }
 
 const getPokemonAbilities = async function(name) {
-    try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-        const pokemon = await response.json();
-        const result = {};
-        result.abilities = pokemon.abilities;
-        result.name = name;
-        return result;
-    } catch(err) {
-
-    }
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const pokemon = await response.json();
+    const result = {};
+    result.abilities = pokemon.abilities;
+    result.name = name;
+    return result;
 }
 
 //TODO modify this to include past type data and maybe correlate to which type it had in which generation
 const getPokemonTypeInfo = async function(name) {
-    try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-        const pokemon = await response.json();
-        const types = pokemon.types;
-        let result = {
-            name: name, 
-            types: []
-        };
-        let type;
-        let typeResult;
-        for (let i = 0; i < types.length; i++) {
-        typeResult = await fetch(types[i].type.url); 
-        type = await typeResult.json(); 
-        console.log(type) 
-        result.types.push({
-                name: types[i].type.name,
-                typeInfo: type
-            });
-        } 
-        return result;
-   } catch(err) {
-    
-   }
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const pokemon = await response.json();
+    const types = pokemon.types;
+    let result = {
+        name: name, 
+        types: []
+    };
+    let type;
+    let typeResult;
+    for (let i = 0; i < types.length; i++) {
+    typeResult = await fetch(types[i].type.url); 
+    type = await typeResult.json(); 
+    console.log(type) 
+    result.types.push({
+            name: types[i].type.name,
+            typeInfo: type
+        });
+    } 
+    return result;
 }
 
 module.exports = {
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -16,20 +16,26 @@ router.get('/findall', async function(req, res, next) {
         const limit = req.query.limit;
         const offset = req.query.offset;
         const range = await getAllPokemonInRange(limit, offset);
+        if (!range) {
+            throw new Error
+        }
         res.send(range);
     } catch(err) {
         res.status(500);
-        res.send(err);
+        res.send({message: "Something went wrong here!"});
     }
 })
 
 router.get('/:name', async function(req, res, next) {
     try {
         const pokemon = await getPokemon(req.params.name);
+        if (!pokemon) {
+            throw new Error;
+        }
         res.send(pokemon);
     } catch(err) {
-        res.status(500);
-        res.send(err);
+        res.status(404); 
+        res.send({message:"pokemon not found"});
     }
     
 });
@@ -37,20 +43,26 @@ router.get('/:name', async function(req, res, next) {
 router.get('/abilities/:name', async function(req, res, next) {
     try {
         const abilities = await getPokemonAbilities(req.params.name);
+        if (!abilities) {
+            throw new Error;
+        }
         res.send(abilities);
     } catch(err) {
-        res.status(500);
-        res.send(err);
+        res.status(404);
+        res.send({message:"pokemon not found"});
     }
 })
 
 router.get('/types/:name', async function(req, res, next) {
     try {
         const types = await getPokemonTypeInfo(req.params.name);
+        if (!types) {
+            throw new Error;
+        }
         res.send(types);
     } catch(err) {
-        res.status(500);
-        res.send(err);
+        res.status(404);
+        res.send({message:"pokemon not found"});
     } 
 })
 
